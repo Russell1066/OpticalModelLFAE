121b1b9df825387c2d1223794b62ee4288e42fb7
Russell1066 <russellwoods1066@gmail.com>
2017-05-02 17:42:58 -0700

    Various adds

--- OpticalModel LFAE/OpticalModel LFAE.cpp
+++ OpticalModel LFAE/OpticalModel LFAE.cpp
@@ -6,15 +6,23 @@
 #include "Tests.h"
 #include "NearField_R00.h"
 #include "VectorMath.h"
+#include "WriteToCSV.h"
 
 using namespace VectorMath;
 
+static const std::string file("C:\\Users\\Lori\\Documents\\I_00.csv");
+
 int main()
 {
-    assert(RunTests());
-    
-    NearField_R00();
-    
+    //assert(RunTests());
+
+    auto t = NearField_R00();
+
+    WriteToCSV(file, t);
+
+    auto max = *t.begin();
+    auto min = max;
+
     return 0;
 }
 
