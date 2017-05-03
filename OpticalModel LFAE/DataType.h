ce66f809a82aa27b4f7be9af51bcc7d943fb4607
Russell1066 <russellwoods1066@gmail.com>
2017-05-02 17:37:42 -0700

    Declare data types to be used running this data
    subject to change

---
+++ OpticalModel LFAE/DataType.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <complex>
+#include "Vector3.h"
+
+typedef double dataType;
+typedef std::complex<dataType> complexType;
+typedef Point3<dataType> pointType;
+typedef std::vector<pointType> pointVector;
